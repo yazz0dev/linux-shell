77247acb276e5ff3586343a9af878bc9f9dc731b
Yaseen k <yzac1569@gmail.com>
2025-04-28 15:26:38 +0530

    Add files via upload

--- R5_prime.sh
+++ R5_prime.sh
@@ -1,17 +1,17 @@
 if [ $# -ne 1 ]; then
 echo "Syntax Error: $0 <number>"
 else
-if [ $1 -le 2 ]; then
-echo "NOT Prime"
+if [ $1 -lt 2 ]; then
+echo "$1 is NOT Prime number"
 exit 1
 fi
 i=2
-while [ $i -lt `expr $1 / 2` ]; do
-i=`expr $i + 1`
+while [ $i -le `expr $1 / 2` ]; do
 if [ `expr $1 % $i` -eq 0 ]; then
 echo "$1 is not a prime number" 
 exit 0
 fi
+i=`expr $i + 1`
 done
 echo "$1 is a prime number"
 fi

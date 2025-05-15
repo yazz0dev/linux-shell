80b6b2c7237c3a23d323da7cf82d745d97af4fe0
yaseen <yzac1569@gmail.com>
2025-05-15 12:58:26 +0530

    upate

--- R14.sh
+++ R14.sh
@@ -1,10 +1,15 @@
+if [ $# -ne 2 ]; then
+echo "Syntax is $0 <file1> <file2>"
+exit 1
+fi
 if [ ! -f $1 ]; then
+echo "$1 isnt existing or not and ordinary file"
 exit 2
 fi
 if [ -f $2 ]; then
 echo "Target file exists overWrite(y/n)?"
 read ans
-if[ $ans -eq "n" ]; then 
+if [ $ans = "n" ]; then 
 exit 3
 fi
 cp $1 $2

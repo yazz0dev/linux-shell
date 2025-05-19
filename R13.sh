99984784bf913a9e076ae71f2fcf13e25fb7dbe1
yaseen <yzac1569@gmail.com>
2025-05-19 15:19:54 +0530

    fix

--- R13.sh
+++ R13.sh
@@ -1,16 +1,15 @@
-if [ $# -le 2 ]; then
+if [ $# -lt 2 ]; then
 echo " Syntax is $0 <search term> <file1>...<filen>"
 exit 1
 fi
-
 word=$1
 shift
 count=0
-while [ $3 -ne 0 ]
+while [ $# -gt 0 ]
 do
-for wd in `cat $1`
+for wd in `cat "$1"`
 do 
-if [ $wd -eq $word ]; then
+if [ $wd = $word ]; then
 count=`expr $count + 1`
 fi
 done

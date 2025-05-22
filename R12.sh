c9850194ff7087a8d52747fe798f17cdf05f8bba
yaseen <yzac1569@gmail.com>
2025-05-22 11:48:31 +0530

    u

--- R12.sh
+++ R12.sh
@@ -5,8 +5,10 @@ fi
 
 if grep -q "$2" "$1"  
 then
+grep -v "$2" "$1" > tmp
+mv tmp "$1"
 echo "$2 deleted from $1 succesfully.\n\nHere is the new file content:"
-grep -v "$2" "$1"
+cat $1
 else
 echo "error deleting $2 from $1"
 fi
--- R18.sh
+++ R18.sh
@@ -1,1 +1,8 @@
-sgdvsfgZZ
+op=0
+while [ op -ne 7 ]; do
+clear
+echo " ---------- "
+read op
+case $opt in 
+1) for f in 
+

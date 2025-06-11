d57eaf5276cc225863446eb5009d5fe98a5e359e
Yaseen k <yzac1569@gmail.com>
2025-06-11 15:24:50 +0530

    Update R8_rev-file.sh

--- R8_rev-file.sh
+++ R8_rev-file.sh
@@ -2,7 +2,6 @@ if [ $# -ne 1 ]; then
 echo "Syntax is $0 <filename>"
 exit 1
 fi
-set -xv
 cp $1 file
 l=`cat file | wc -l`
 rm rfile
@@ -13,3 +12,6 @@ l=`expr $l - 1`
 head -n $l file > temp
 mv temp file
 done
+
+echo "reversed file content: "
+cat rifle 
